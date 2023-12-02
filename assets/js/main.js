9fedbec24bccaa5b7c959c183387edbef8b2f37e
rianadoctor <ridoctor@davidson.edu>
2023-12-02 17:07:22 -0500

    add-bootstrap
    adding bootstrap format to shop.htm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
 document.getElementById('card').addEventListener('click', function () {
+    // Toggle the 'flipped' class on the card
     this.classList.toggle('flipped');
+
+    // Get the button element
+    var button = document.querySelector('.button');
+
+    // Toggle the visibility of the button based on the 'flipped' class
+    if (this.classList.contains('flipped')) {
+        button.style.display = 'block'; // Show the button
+    } else {
+        button.style.display = 'block'; // Hide the button
+    }
+});
+
+
+
+document.getElementById('searchInput').addEventListener('input', function() {
+    this.value = 'hat' + this.value.slice(this.value.lastIndexOf(' '));
 });
