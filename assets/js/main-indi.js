3000a9974b8cef75a2c1d9d55dad7beb768de16d
rianadoctor <ridoctor@davidson.edu>
2023-12-03 13:38:14 -0500

    adding-icon-buy
    updating icon and buy function

--- assets/js/main-indi.js
+++ assets/js/main-indi.js
@@ -15,12 +15,17 @@ function updateProductDetails(productId) {
     document.getElementById('subtotal').textContent = `$${productDetails.subtotal.toFixed(2)}`;
     document.getElementById('shipping').textContent = `$${productDetails.shipping.toFixed(2)}`;
     document.getElementById('total').textContent = `$${productDetails.total.toFixed(2)}`;
-    document.getElementById('checkoutAmount').textContent = `$${productDetails.total.toFixed(2)}`;
     document.getElementById('price').textContent = `$${parseInt(productDetails.price, 10)}`;
 
     // Additional text updates
     document.getElementById('name').textContent = productDetails.name;
     document.getElementById('description').textContent = productDetails.description;
+
+
+
+    // Updating image source 
+    document.getElementById('productImage').src = '../assets/img/' + productDetails.imageFileName; 
+
 }
 
 function getProductDetailsById(productId) {
@@ -28,9 +33,17 @@ function getProductDetailsById(productId) {
     const key = String(productId);
 
     const productDetails = {
-        1: { subtotal: 498.00, shipping: 2.00, total: 4818.00, price: 500, name: 'Vacuum', description: '256GB, Navy Blue' },
-        // Add more products as needed
+        1: { subtotal: 98.00, shipping: 2.00, total: 4818.00, price: 50, name: 'Vacuum', description: 'Super Pixel Vacuum', imageFileName:'shop-1.png' },
+        2: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-2.png' },
+        3: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-3.png' },
+        4: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-4.png' },
+        5: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-5.png' },
+        6: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-6.png' },
+        7: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-7.png' },
+        8: { subtotal: 150.00, shipping: 100.00, total: 250.00, price: 150, name: 'Phone', description: 'Super Pixel Phone', imageFileName:'shop-8.png' },
     };
+
+    
 
     // Convert productId to string to handle both numeric and string input
     return productDetails[key] || null;
